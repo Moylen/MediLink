354e2fb33cdd6f5335d9dc59944bfce699a99dd1
Moylen <90926227+Moylen@users.noreply.github.com>
2024-05-04 22:11:10 +0700

    Fix: care-result api

--- server/src/care-records/care-records.module.ts
+++ server/src/care-records/care-records.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CareRecord } from './entities/care-record.entity';
 import { AuthModule } from '../auth/auth.module';
 import { CaresModule } from './cares/cares.module';
+import { CareResultsModule } from './care-result/care-results.module';
 
 @Module({
   controllers: [CareRecordsController],
@@ -13,6 +14,7 @@ import { CaresModule } from './cares/cares.module';
     TypeOrmModule.forFeature([CareRecord]),
     AuthModule,
     CaresModule,
+    CareResultsModule
   ]
 })
 export class CareRecordsModule {}
