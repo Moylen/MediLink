ce501305438a93d402d062cf9f4d1e8a7805b320
Moylen <90926227+Moylen@users.noreply.github.com>
2024-05-28 18:32:01 +0700

    Rollback

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PatientsModule } from './patients/patients.module';
 import { dataSourceOptions } from '../db/data-source';
